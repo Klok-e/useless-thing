a344cb18ca1111e82a30f58beb6d747253de10e7
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2022-08-22 19:04:59 +0300

    init

---
+++ benches/reverse_words.rs
@@ -0,0 +1,23 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+pub fn reverse_words(s: String) -> String {
+    s.split_whitespace()
+        .map(|s: &str| s.chars().rev().chain(std::iter::once(' ')))
+        .flatten()
+        .take(s.len())
+        .collect::<String>()
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let text = std::fs::read_to_string("lorem.txt").unwrap();
+    c.bench_with_input(BenchmarkId::new("reverse", ""), &text, |b, i| {
+        b.iter_batched(
+            || i.clone(),
+            |i| reverse_words(i),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
